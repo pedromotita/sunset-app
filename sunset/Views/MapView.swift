5b558354413f5928f2c5575cc5914c29c9e3fd6c
pedromotita <p.smota98@gmail.com>
2022-03-09 09:35:48 -0300

    Added simple MapAnnotation of a sunset for the Map

--- sunset/Views/MapView.swift
+++ sunset/Views/MapView.swift
@@ -12,12 +12,26 @@ struct MapView: View {
     
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: -22.8045260, longitude: -47.0812497),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        span: MKCoordinateSpan(latitudeDelta: 0.8, longitudeDelta: 0.8)
     )
+    
+    private var sunsets = [Sunset(location: CLLocationCoordinate2D(latitude: -22.8318632, longitude: -47.0605383))]
     
     var body: some View {
         NavigationView {
-            Map(coordinateRegion: $region).edgesIgnoringSafeArea(.bottom)
+            Map(
+                coordinateRegion: $region,
+                annotationItems: sunsets
+            ) { sunset in
+                MapAnnotation(coordinate: sunset.location) {
+                    Circle()
+                        .foregroundColor(.red)
+                        .onTapGesture {
+                            print("tapped")
+                        }
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
             .navigationTitle("Sunset")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
